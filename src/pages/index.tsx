3135c85076bfad52cb46b5c9cc5fade5af05136b
Marnoux <marnouxmanser@gmail.com>
2022-12-06 10:49:19 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import { Header } from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>{`Marnoux's Portfolio`}</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </div>
+  );
+}
