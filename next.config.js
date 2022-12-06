7c3786803d84dc64a9e1ce6ccfde1741ef734e43
Marnoux <marnouxmanser@gmail.com>
2022-12-06 14:50:06 +0200

    Finish hero and header

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['marnoux.github.io'],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import { BackgroundCircles } from './BackgroundCircles';
 
 type Props = {};
 
@@ -6,7 +9,7 @@ export const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
       "Hi, I'm Marnoux", //
-      "<I'm a web developer />",
+      "I'm a <WebDeveloper />",
       "I'm a father",
       "I'm a husband",
     ],
@@ -15,11 +18,36 @@ export const Hero = (props: Props) => {
   });
 
   return (
-    <div>
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        alt="heroImg"
+        className="relative rounded-full w-32 h-32 object-cover"
+        height={320}
+        width={320}
+        src="https://marnoux.github.io/assets/img/hero-img.png"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Front-End Engineer</h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div>
+          <Link href={'#about'}>
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href={'#experience'}>
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href={'#skills'}>
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href={'#projects'}>
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
