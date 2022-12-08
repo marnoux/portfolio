8c1dc34910a0657132241e8dfd9a9493d6bb8d56
Marnoux <marnouxmanser@gmail.com>
2022-12-08 23:29:17 +0200

    Add Projects page

---
+++ src/components/Projects.tsx
@@ -0,0 +1,52 @@
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+export const Projects = () => {
+  const projects = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Projects</h3>
+
+      <div className="relative flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div
+            key={i}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p20 m:p-44 h-screen content-center"
+          >
+            <motion.div
+              initial={{ y: -300, opacity: 0 }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+            >
+              <Image
+                alt={i.toString()}
+                src={'https://marnoux.github.io/assets/img/react.png'}
+                height={666}
+                width={375}
+              />
+            </motion.div>
+
+            <div>
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#F7AB0A]/40">
+                  Case Study {i + 1} of {projects.length}:
+                </span>
+                UPS Clone
+              </h4>
+
+              <p className="text-lg text-center md:text-left p-10">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et explicabo doloribus
+                distinctio repellendus odit sunt exercitationem, consequatur odio officiis velit,
+                tempora voluptatum numquam autem. Dolores quisquam illo odit rem non.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { About } from '../components/About';
 import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
+import { Projects } from '../components/Projects';
 import { Skills } from '../components/Skills';
 import { WorkExperience } from '../components/WorkExperience';
 
@@ -35,7 +36,9 @@ export default function Home() {
       </section>
 
       {/* Projects */}
-
+      <section id="projects" className="snap-center">
+        <Projects />
+      </section>
       {/* Contact Me */}
     </div>
   );
