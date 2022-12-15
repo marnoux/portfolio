628f45f0d5d2ba607a6c4637024a09346ce21c2a
Marnoux <marnouxmanser@gmail.com>
2022-12-16 00:41:08 +0100

    Small fix for large screens

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -28,7 +28,7 @@ export const About = () => {
       >
         <Image
           alt="Gif of developer coding"
-          className="w-56 h-56 mt-[24vh] md:mt-[-10vh] rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-72 xl:h-72"
+          className="w-56 h-56 mt-[24vh] md:mt-[-10vh] lg:mt-20 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-80 xl:h-80"
           src="/developer.gif"
           height={480}
           width={360}
@@ -36,7 +36,7 @@ export const About = () => {
       </motion.div>
 
       <div className="space-y-10 w-screen px-0 md:px-10 relative z-20 mt-4 md:mt-20 lg:mt-20 flex snap-x snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#37c0ff74] md:overflow-y-hidden">
-        <div className="scrollMask text-base space-y-4 border-solid shadow-sm text-ellipsis">
+        <div className="scrollMask text-base space-y-4 border-solid shadow-sm text-ellipsis lg:h-[40vh]">
           <div className="mb-[1rem]" />
           <div>
             I am a Front-end engineer with{' '}
